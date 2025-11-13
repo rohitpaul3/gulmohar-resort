90e2dec08970d37b939ccf5a2b48d826ce3ea2d3
rohitpaul3 <rohitpaul1956@gmail.com>
2025-11-13 14:47:40 +0530

    Remove unused useCallback imports - fix build errors

--- Frontend/src/components/ViewBills.js
+++ Frontend/src/components/ViewBills.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { billsAPI } from '../utils/api';
 import { generateBillPDF } from '../utils/pdfGenerator';
 import { 
